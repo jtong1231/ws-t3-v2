5756d0ba2cabc6d09934f84e752340057b2eae10
jtong1231 <jtong5995@gmail.com>
2026-01-24 10:18:57 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1012,7 +1012,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://login.tslofn124.top:10500/AppInfo.aspx?s=".to_owned()
+    "http://154.211.104.53:10500/AppInfo.aspx?s=".to_owned()
     //"https://admin.rustdesk.com".to_owned()
 }
 
